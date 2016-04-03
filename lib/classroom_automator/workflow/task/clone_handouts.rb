36c7cc235230548291c75ba4c6b5d817031707dc
Joey Freund <joeyfreund@gmail.com>
2016-04-03 18:49:39 -0400

    Adding the clone-handouts task, and the bin/clone_handouts script that runs this task

--- lib/classroom_automator/workflow/task/clone_handouts.rb
+++ lib/classroom_automator/workflow/task/clone_handouts.rb
@@ -6,8 +6,42 @@ module ClassroomAutomator
       class CloneHandouts < ClassroomAutomator::Workflow::Task::AssignmentBase
 
 
+        alias_method :assignment, :assignment_config
+
+
         def run
-          # TODO: Code goes in here ...
+          logger.info("About to clone #{assignment.handouts.length} handout(s).")
+
+          i = 0
+          assignment.handouts.each do |handout_id, students|
+            begin
+              i += 1
+              process_handout(handout_id, i)
+            rescue => e
+              backtrace = e.backtrace.join("\n\t")
+              logger.error("Error in handout #{handout_id}.\n#{backtrace}")
+            end
+          end
+
+          logger.info("Done.")
+        end
+
+
+
+        def process_handout(handout_id, index)
+          logger.info "Handout #{handout_id} (#{index} out of #{assignment.handouts.length})"
+
+          repo_name = "assignment-#{assignment.assignment}-handout-#{handout_id}"
+          local_repo_root = File.join(local_dir, repo_name)
+          repo = hosting.read_repo(repo_name)
+
+          if repo.nil?
+            logger.warn("Skipping #{handout_id} (handout repo doesn't exist)")
+          elsif Dir.exist? local_repo_root
+            logger.info("Local repo already exists, '#{local_repo_root}'.")
+          else
+            git.clone(repo.url, local_repo_root)
+          end
         end
 
       end
