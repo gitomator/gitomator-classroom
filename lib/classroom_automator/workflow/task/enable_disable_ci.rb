08ef8f2bfdb499c2c5e74ac6285f903d53100e91
Joey Freund <joeyfreund@gmail.com>
2016-04-04 12:04:54 -0400

    Adding tasks (and scripts in the bin/ folder) to enable/disable CI for all handouts in an assignment

---
+++ lib/classroom_automator/workflow/task/enable_disable_ci.rb
@@ -0,0 +1,60 @@
+require 'classroom_automator/workflow/task/assignment_base'
+
+module ClassroomAutomator
+  module Workflow
+    module Task
+
+      #
+      # Common module to be included in both EnableCI and DisableCI classes
+      #
+      module EnableDisableCI
+
+        def initialize(context, assignment_config)
+          super(context, assignment_config, nil)
+        end
+
+        def run
+          sync
+          super()
+          logger.info "Done."
+        end
+
+        def sync
+          logger.info "Syncing CI ..."
+          ci.sync
+          while ci.syncing?
+            print "."
+            sleep 1
+          end
+          logger.info "CI synchronized"
+        end
+
+      end
+
+
+
+      class EnableCI < ClassroomAutomator::Workflow::Task::AssignmentBase
+        include EnableDisableCI
+
+        def process_handout(repo_name, i)
+          super
+          ci.enable_ci repo_name
+        end
+
+      end
+
+
+      class DisableCI < ClassroomAutomator::Workflow::Task::AssignmentBase
+        include EnableDisableCI
+
+        def process_handout(repo_name, i)
+          super
+          ci.disable_ci repo_name
+        end
+
+      end
+
+
+    end
+  end
+end
