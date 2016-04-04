1125dea0a9a84a41d69a474a50f87cb192277ed6
Joey Freund <joeyfreund@gmail.com>
2016-04-04 15:30:37 -0400

    Adding the create-handout-repos task

---
+++ lib/classroom_automator/workflow/task/create_handout_repos.rb
@@ -0,0 +1,35 @@
+require 'classroom_automator/workflow/task/assignment_base'
+
+module ClassroomAutomator
+  module Workflow
+    module Task
+      class CreateHandoutRepos < ClassroomAutomator::Workflow::Task::AssignmentBase
+
+
+        def initialize(context, assignment_config)
+          super(context, assignment_config, nil)
+        end
+
+
+        def run
+          logger.info "About to create #{handouts.length} handout repo(s) ..."
+          super()
+          logger.info "Done."
+        end
+
+
+        def process_handout(repo_name, i)
+          super
+          if (hosting.read_repo(repo_name).nil?)
+            hosting.create_repo(repo_name)
+          else
+            logger.info "Skipping #{repo_name}, repo already exists."
+          end
+        end
+
+
+
+      end
+    end
+  end
+end
