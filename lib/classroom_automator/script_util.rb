ec7b481fdc46cd96c425b8bb6b5e4f07af68c776
Joey Freund <joeyfreund@gmail.com>
2016-04-05 11:42:01 -0400

    A slightly more sensible organization of the script code and helpers.

---
+++ lib/classroom_automator/script_util.rb
@@ -0,0 +1,75 @@
+require 'trollop'
+require 'classroom_automator/version'
+require 'classroom_automator/workflow/context'
+require 'classroom_automator/config/assignment'
+
+
+module ClassroomAutomator
+  module ScriptUtil
+
+    DEFAULT_CONTEXT_ENV_VAR_NAME = 'CLASSROOM_AUTOMATOR_CONTEXT'
+
+    def self.default_context_file
+      ENV[DEFAULT_CONTEXT_ENV_VAR_NAME]
+    end
+
+    #---------------------------------------------------------------------------
+
+    class DefaultOptionParser < Trollop::Parser
+
+      def initialize(usage_message)
+        super
+        version "Classroom-automatoer version #{ClassroomAutomator::VERSION}"
+        usage   usage_message
+
+        opt :context,
+              "Context (logger, hosting, git, ci) configuration file. ",
+              :default =>  ScriptUtil::default_context_file || "#{DEFAULT_CONTEXT_ENV_VAR_NAME} env variable, currently not set.",
+              :type => :string
+      end
+
+      def parse(args)
+        return { :context => ScriptUtil::default_context_file }.merge(
+          Trollop::with_standard_exception_handling(self) { super(args)  }
+        )
+      end
+
+    end
+
+    #---------------------------------------------------------------------------
+
+
+    def self.run_task(task)
+      begin
+        task.run
+      rescue => e
+        abort "ERROR: #{e}.\n\n#{e.backtrace.join("\n\t")}"
+      end
+    end
+
+
+    def self.task_with_context_config_assignment_config_and_local_dir(
+      task_class, context_conf_file, assignment_conf_file, local_dir)
+
+      task_class.new(
+        ClassroomAutomator::Workflow::Context.from_file(context_conf_file),
+        ClassroomAutomator::Config::Assignment.from_file(assignment_conf_file),
+        local_dir
+      )
+    end
+
+    def self.task_with_context_config_and_assignment_config(
+      task_class, context_conf_file, assignment_conf_file)
+
+      task_class.new(
+        ClassroomAutomator::Workflow::Context.from_file(context_conf_file),
+        ClassroomAutomator::Config::Assignment.from_file(assignment_conf_file)
+      )
+    end
+
+
+    #---------------------------------------------------------------------------
+
+
+  end
+end
