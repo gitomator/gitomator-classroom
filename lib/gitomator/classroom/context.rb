25b7bc3358efe51074c5cb87ec664d994c67af3a
Joey Freund <joeyfreund@gmail.com>
2016-04-15 16:01:08 -0400

    Adding the tagging accessor to the context

--- lib/gitomator/classroom/context.rb
+++ lib/gitomator/classroom/context.rb
@@ -2,6 +2,7 @@ require 'gitomator/classroom'
 
 require 'gitomator/context'
 require 'gitomator/service/hosting/service'
+require 'gitomator/service/tagging/service'
 require 'gitomator/service/ci/service'
 
 
@@ -22,6 +23,15 @@ module Gitomator
           create_ci_service(ci_config || {})
         end
 
+        register_service_factory :tagging do |tagging_config|
+          create_tagging_service(tagging_config || {})
+        end
+
+      end
+
+
+      def tagging
+        @tagging ||= create_service(:tagging)
       end
 
 
@@ -49,6 +59,17 @@ module Gitomator
             Gitomator::Travis::CIProvider.from_config(config))
         else
           raise "Cannot create CI service - Invalid configuration, #{config}."
+        end
+      end
+
+
+      def create_tagging_service(config)
+        if config['provider'] == 'github'
+          require 'gitomator/github/tagging_provider'
+          return Gitomator::Service::Tagging::Service.new (
+            Gitomator::GitHub::TaggingProvider.from_config(config))
+        else
+          raise "Invalid hosting provider in service configuration - #{config}"
         end
       end
 
