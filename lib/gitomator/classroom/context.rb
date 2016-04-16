40f31a37e7ba18580fcaa76771d3ae0a595c70e9
Joey Freund <joeyfreund@gmail.com>
2016-04-16 01:25:56 -0400

    Cleaning up the context - Just define the various create_PROVIDER_SERVICE_methods, and everything else happens auto-magically

--- lib/gitomator/classroom/context.rb
+++ lib/gitomator/classroom/context.rb
@@ -1,10 +1,5 @@
 require 'gitomator/classroom'
-
 require 'gitomator/context'
-require 'gitomator/service/hosting/service'
-require 'gitomator/service/tagging/service'
-require 'gitomator/service/ci/service'
-
 
 
 module Gitomator
@@ -12,60 +7,31 @@ module Gitomator
     class Context < Gitomator::Context
 
 
-      def initialize(config)
-        super(config)
-
-        register_service_factory :hosting do |hosting_config|
-          create_hosting_service(hosting_config || {})
-        end
-
-        register_service_factory :ci do |ci_config|
-          create_ci_service(ci_config || {})
-        end
-
-        register_service_factory :tagging do |tagging_config|
-          create_tagging_service(tagging_config || {})
-        end
-
+      def create_github_hosting_service(config)
+        require 'gitomator/service/hosting/service'
+        require 'gitomator/github/hosting_provider'
+        return Gitomator::Service::Hosting::Service.new (
+          Gitomator::GitHub::HostingProvider.from_config(config))
       end
 
 
+      def create_travis_ci_service(config)
+        require 'gitomator/service/ci/service'
+        require 'gitomator/travis/ci_provider'
+        return Gitomator::Service::CI::Service.new(
+          Gitomator::Travis::CIProvider.from_config(config))
+      end
 
-      def create_hosting_service(config)
-        case config['provider']
-        when nil
-          return create_default_hosting_service(config)
-        when 'local'
-          return create_default_hosting_service(config)
-        when 'github'
-          require 'gitomator/github/hosting_provider'
-          return Gitomator::Service::Hosting::Service.new (
-            Gitomator::GitHub::HostingProvider.from_config(config))
-        else
-          raise "Invalid hosting provider in service configuration - #{config}"
-        end
+      def create_travis_pro_ci_service(config)
+        create_travis_ci_service(config)
       end
 
 
-      def create_ci_service(config)
-        if ['travis', 'travis_pro'].include? config['provider']
-          require 'gitomator/travis/ci_provider'
-          return Gitomator::Service::CI::Service.new(
-            Gitomator::Travis::CIProvider.from_config(config))
-        else
-          raise "Cannot create CI service - Invalid configuration, #{config}."
-        end
-      end
-
-
-      def create_tagging_service(config)
-        if config['provider'] == 'github'
-          require 'gitomator/github/tagging_provider'
-          return Gitomator::Service::Tagging::Service.new (
-            Gitomator::GitHub::TaggingProvider.from_config(config))
-        else
-          raise "Invalid hosting provider in service configuration - #{config}"
-        end
+      def create_github_tagging_service(config)
+        require 'gitomator/service/tagging/service'
+        require 'gitomator/github/tagging_provider'
+        return Gitomator::Service::Tagging::Service.new (
+          Gitomator::GitHub::TaggingProvider.from_config(config))
       end
 
 
