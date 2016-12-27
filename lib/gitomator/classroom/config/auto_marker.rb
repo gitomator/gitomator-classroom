7901620d8e901f8f6c05cc0a2b2c97a25f5dc3bd
Joey Freund <joeyfreund@gmail.com>
2016-12-26 20:26:53 -0500

    update auto-marker-config - Getting rid of all the fancy Ruby meta-programming stuff

--- lib/gitomator/classroom/config/auto_marker.rb
+++ lib/gitomator/classroom/config/auto_marker.rb
@@ -1,14 +1,52 @@
-require 'gitomator/classroom/config/assignment'
+require 'gitomator/task/config/repos_config'
+
 
 module Gitomator
   module Classroom
     module Config
-      class AutoMarker < Gitomator::Classroom::Config::Assignment
+      class AutoMarker < Gitomator::Task::Config::ReposConfig
 
-        property :docker_image, {:required => true}
-        property :automarker_script, {:required => true, :is_executable => true }
-        property :results_dir, {:required => true, :is_dir => true}
-        property :resources, {:default => []}
+
+        attr_accessor :docker_image
+        attr_accessor :automarker_script
+        attr_accessor :resources
+        attr_accessor :env
+        
+
+        #
+        # @param config_obj [Hash] Configuration data (commonly loaded from a YAML file)
+        #
+        def initialize(config_obj)
+          super(config_obj)
+
+          @docker_image      = config_obj['docker_image']
+          @automarker_script = File.expand_path config_obj['automarker_script']
+          @resources         = (config_obj['resources'] || {})
+          @resources = @resources.map {|r,p| [r, File.expand_path(p)]} .to_h
+
+          @env               = config_obj['env'] || {}
+        end
+
+
+        def validate()
+          raise "Missing docker_image" if docker_image.nil?
+          raise "Missing automarker_script" if automarker_script.nil?
+
+          required(automarker_script)
+          unless File.executable? automarker_script
+            raise "Not executable #{automarker_script} "
+          end
+
+          resources.each do |_, path|
+            required(path)
+          end
+        end
+
+        def required(path)
+          unless File.exist?(File.expand_path(path))
+            raise "No such file/directory #{path} "
+          end
+        end
 
       end
     end
